757190f4132feb290b05aa20804d4379d2763c99
Santhosh <santhoshvenkateswaran.s@gmail.com>
2022-06-18 19:05:04 +0530

    use static json for words

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,23 +15,45 @@ export class AppComponent implements OnInit {
 
   word$ = new BehaviorSubject<string>('loading...');
 
+  wordList: string[] = [];
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.generateWord().subscribe(word => this.word$.next(word));
-    this.interval$.pipe(
-      debounceTime(500),
-      distinctUntilChanged(),
-      switchMap((intervalInSeconds: number) => interval(intervalInSeconds * 1000)),
-      switchMap(() => this.generateWord())
-    ).subscribe((word) => this.word$.next(word));
+    this.fetchWordList().subscribe((words) => {
+      this.wordList = words;
+      this.generateWord().subscribe(word => this.word$.next(word));
+      this.interval$.pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap((intervalInSeconds: number) => interval(intervalInSeconds * 1000)),
+        switchMap(() => this.generateWord())
+      ).subscribe((word) => this.word$.next(word));
+    })
   }
 
   onIntervalChange($event: number): void {
     this.interval$.next($event);
   }
 
+  // private generateWord(): Observable<string> {
+  //   return this.http.get<string[]>('https://random-word-form.herokuapp.com/random/noun').pipe(map(response => response[0]));
+  // }
+
+  private fetchWordList(): Observable<string[]> {
+    return this.http.get<string[]>('/assets/words.json');
+  }
+
   private generateWord(): Observable<string> {
-    return this.http.get<string[]>('https://random-word-form.herokuapp.com/random/noun').pipe(map(response => response[0]));
+    return new Observable(subscriber => {
+      const i = Math.floor((Math.random() * this.wordList.length));
+      subscriber.next(this.wordList[i]);
+      subscriber.complete();
+    });
+    // return this.http.get<string[]>('/assets/words.json').pipe(map(response => {
+    //   const l = response.length;
+    //   const i = Math.floor((Math.random() * l - 1) + 0);
+    //   return response[i];
+    // }))
   }
 }
